f601f8c29bab81d1fbf38b25fdbe60302618e6a2
L1NINE <939343350@qq.com>
2025-04-06 21:44:49 +0800

    新增通用的角色数值组件NPCBattleValueManager跟配套的简单生命UI
    整了通用的角色数值组件NPCBattleValueManager
    (引用的时候简称nbvm)跟配套的简单生命UI

---
+++ Assets/Scripts/BattleSystem/NPCBattleValueManager.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NPCBattleValueManager : MonoBehaviour
+{
+    public enum NPCType
+    {
+        None,
+        Player,
+        NormalEnemy,
+        Boss
+    }
+    //储存数值的脚本，主角与NPC通用
+    [Header("战斗数值")]
+    public float MaxHP = 5;
+    public float CurrentHP = 5;
+    public float Attack = 1;
+
+    public bool canTakeDamage = true;
+    
+    [Header("位移数值")]
+    public float MoveSpeed = 5;
+    public float JumpForce = 180;
+
+    [Header("AI数值")]
+    public float AIAttackInterval = 1;//攻击间隔
+
+    [Header("阵营")]
+    public NPCType npcType;
+
+    #region 私有变量
+    [Header("私有变量")]
+    private Rigidbody2D rb;
+    public bool isHit = false;
+
+    #endregion
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    #region 碰撞检测函数
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (npcType)
+        {
+            case NPCType.Player://玩家碰撞检测
+            return;
+
+            case NPCType.NormalEnemy://普通敌人碰撞检测
+            if (collision.gameObject.CompareTag("Wave"))
+            {
+                isHit = true;
+                TakeDamage(1);
+                Destroy(collision);
+            }
+            else
+            {
+                isHit = false;
+            }
+            return;
+
+            case NPCType.Boss://Boss碰撞检测
+            return;
+
+            default:
+            return;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (npcType)
+        {
+            case NPCType.Player://玩家碰撞检测
+            return;
+
+            case NPCType.NormalEnemy://普通敌人碰撞检测
+            return;
+
+            case NPCType.Boss://Boss碰撞检测
+            return;
+
+            default:
+            return;
+        }
+    }
+
+    #endregion
+
+
+
+
+    #region 功能函数
+
+    #region 共有函数
+    public void DeathJudge()
+    {
+        if (CurrentHP <= 0)
+        {
+            if (npcType == NPCType.Player)
+            {
+                Player_Death();
+            }
+            else
+            {
+                Enemy_Die();
+            }
+        }
+    }
+
+    #region 普通敌人函数区域
+    public void TakeDamage(int damage)
+    {
+        if (canTakeDamage)
+        {
+            CurrentHP -= damage;
+            isHit = true;
+            //canTakeDamage = false;
+        }
+        if (CurrentHP <= 0)
+        {
+            Enemy_Die();
+        }
+    }
+
+        void Enemy_Die()
+    {
+        Destroy(gameObject);
+    }
+    
+    #endregion
+    #region 玩家函数区域
+
+    private void Player_Death()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        CameraFollow camera = Camera.main.GetComponent<CameraFollow>();
+    
+        if (camera != null)
+        {
+            //camera.StopFollowing();
+        }
+    }
+
+    private void Restart()//死亡后重新加载场景
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+
+
+    #endregion
+    #endregion
+    #endregion
+}
